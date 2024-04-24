122b0df8a6d78fa3ba7a270dd190ff4634d30424
Guilherme <guilhermeganim@hotmail.com>
2024-04-24 11:51:06 -0300

    add: Fastify server / docker / prisma / project structury schema and migration

--- apps/api/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { defineAbilityFor } from '@saas/auth'
-
-const ability = defineAbilityFor({ role: 'MEMBER', id: 'user-id' })
-const userCanInviteSomeoneElse = ability.can('invite', 'User')
-
-console.log(userCanInviteSomeoneElse)
